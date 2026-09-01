54c5b3590fb79483ee229f5b3780e77a72f01958
Brielle Hamel <briellenyc8@gmail.com>
2026-09-01 15:08:16 -0400

    Add files via upload

---
+++ swirl1-1.R
@@ -0,0 +1,26 @@
+#Swirl Lesson 1 Brielle Hamel
+library('swirl')
+swirl()
+BrielleNycole
+1
+#Q1 (Storing Variables)
+5+7
+x <- 5+7
+x
+y <- x-3
+y
+z <- c(1.1,9,3.14) #Creating a vector aka small collection of numbers
+?c
+z
+c(z,555,z)
+z*2+1000
+my_sqrt <- sqrt(z-1)
+2
+my_sqrt
+my_div <- z/my_sqrt
+1
+my_div #The first element of my_div is equal to the first element of z divided by the first element of my_sqrt, and so on...
+c(1,2,3,4)+c(0,10)
+c(1,2,3,4)+c(0,10,100)
+my_div #start typing word and press tab to find
+2
