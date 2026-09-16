3a4eac9c0f6a3e59dfa2c32d9be69bde6f730ddb
Brielle Hamel <briellenyc8@gmail.com>
2026-09-16 11:47:19 -0400

    Add files via upload

--- Master.R
+++ Master.R
@@ -1,6 +1,22 @@
 #Master Script
 
+xj <- c(0, 0, 1, 1)         
+yj <- c(0, 1, 0, 1)          
+pj <- c(0.4, 0.1, 0.1, 0.4)  
+EX  <- sum(xj*pj)            
+EY  <- sum(yj*pj)           
+EXY <- sum(xj*yj*pj)        
+Cov <- EXY - EX*EY          
+EX2 <- sum((xj^2)*pj)
+EY2 <- sum((yj^2)*pj)
+VarX <- EX2 - EX^2           
+VarY <- EY2 - EY^2           
+sdX <- sqrt(VarX)            
+sdY <- sqrt(VarY)            
+Cor <- Cov / (sdX*sdY)
+
 #Var(aX+b) = a^2(VarX)
 
-#Var(aX+bY) = a^2(VarX) - b^2(VarY) + 2ab(Cov(x,y))
+#Var(aX+bY) = (a^2)(VarX) - (b^2)(VarY) + 2ab(Cov(x,y))
 
+#E[aX + bY] = a*E[X] + b*E[Y] 
